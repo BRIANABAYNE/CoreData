a3665bbfcb8acb6d0088c08011b83e7fe517b067
Briana Bayne <brianabayne1@gmail.com>
2024-02-19 20:24:45 -0700

    Added outlets for detail screen.

--- CoreData3/CoreData3/DetailViewController.swift
+++ CoreData3/CoreData3/DetailViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    
+    @IBOutlet weak var coreDataImage: UIImageView!
+    @IBOutlet weak var coreDataNameTextField: UITextField!
+    @IBOutlet weak var coreDataArtistTextField: UITextField!
+    @IBOutlet weak var coreDataYearTextField: UITextField!
+    
+    // MARK: - Lifecycle's
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+// MARK: - Actions
+    
+    @IBAction func coreDataSaveButtonTapped(_ sender: Any) {
     }
-    */
 
 }
