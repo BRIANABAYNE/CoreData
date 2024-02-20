7a4880d312af5050a068aee882f80ea4ffd99d49
Briana Bayne <brianabayne1@gmail.com>
2024-02-19 19:55:25 -0700

    Initial commit.

---
+++ CoreData3/CoreData3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData3
+//
+//  Created by Briana Bayne on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
